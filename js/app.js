9fa135e48dcb598af551b6fcc3df11bbbd1ed383
lwenke01 <lwenke@gmail.com>
2016-01-20 14:40:09 -0800

    got my first chart to populate. woot

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 "use strict"
 //create object constructor for products, and Global allClicks counter
+// var
+var productArray = []
 function Product(name, fileLoc) {
   this.name = name;
   this.fileLoc = fileLoc;
   this.timesClicked = 0;
   this.timesDisplayed = 0;
-  this.percentClicked = 0;
   this.percentClicked = function() {
-  this.percentClicked = (timesClicked / timesDisplayed).toFixed(2) * 100;
+  return(this.timesClicked / this.timesDisplayed).toFixed(2) * 100;
   }
+  productArray.push(this);
 }
 
 var shark = new Product('shark', 'img/shark.png');
@@ -26,125 +28,133 @@ var water = new Product('water-can','img/water.png');
 var wine = new Product('wine-glass','img/wine.png');
 var chair = new Product('chair','img/chair.png');
 
-var nameArray = [shark, chair, pen, banana, bag, boots, cthulhu, dragon, sweep, scissors, unicorn, usb, water, wine];
+
 
 
 var img1 = document.getElementById('productInsert1');
 var img2 = document.getElementById('productInsert2');
 var img3 = document.getElementById('productInsert3')
 var totalClicks = 0;
-var clickOne = 0;
-var clickTwo = 0;
-var clickThree = 0;
+
+// var timesDisplayed = 0;
 var rand1;
 var rand2;
 var rand3;
 
 //generate a random image from array
 function generateRandomImage() {
-  return Math.floor((Math.random() * nameArray.length));
+  return Math.floor((Math.random() * productArray.length));
 }
 
  //constuctors to generate 3 random images
 function random() {
   rand1 = generateRandomImage();
   console.log(rand1 + "This is rand1");
-  img1.src = nameArray[rand1].fileLoc;
-  // this.displayedClicks =
+  img1.src = productArray[rand1].fileLoc;
+  productArray[rand1].timesDisplayed += 1;
 
   rand2 = generateRandomImage();
   while(rand1 === rand2) {
     rand2 = generateRandomImage();
-}
-  img2.src = nameArray[rand2].fileLoc;
+  }
+  img2.src = productArray[rand2].fileLoc;
+  productArray[rand2].timesDisplayed += 1;
   console.log(rand2 + "This is rand2");
   rand3 = generateRandomImage();
   while(rand1 === rand3 || rand2 === rand3) {
     rand3 = generateRandomImage();
   }
-  img3.src = nameArray[rand3].fileLoc;
-    console.log(rand3 + "This is rand3");
+  img3.src = productArray[rand3].fileLoc;
+  productArray[rand3].timesDisplayed += 1;
+  console.log(rand3 + "This is rand3");
  }
 //call the random image
 random();
 
+//event listeners for all 3 displays
+productInsert1.addEventListener('click', handleClickOnOne);
+productInsert2.addEventListener('click', handleClickOnTwo);
+productInsert3.addEventListener('click', handleClickOnThree);
 
 //event handlers to create event for when image is clicked on
 function handleClickOnOne(event) {
-  clickOne += 1;
-  console.log(event);
   totalClicks += 1;
-  nameArray[rand1].timesClicked += 1;
-  random();
+  productArray[rand1].timesClicked += 1;
   checkForButton();
-  makeClickArray();
+  random();
 }
 function handleClickOnTwo(event) {
-  clickTwo += 1;
-  console.log(event);
   totalClicks += 1;
-  nameArray[rand2].timesClicked += 1;
-  random();
+  productArray[rand2].timesClicked += 1;
   checkForButton();
-  makeClickArray();
+  random();
 }
 function handleClickOnThree(event) {
-  clickThree += 1;
   totalClicks += 1;
-  nameArray[rand3].timesClicked += 1;
-  random();
+  productArray[rand3].timesClicked += 1;
   checkForButton();
-  makeClickArray();
+  random();
 }
-//event listeners for event handlers
-  productInsert1.addEventListener('click', handleClickOnOne);
-  productInsert2.addEventListener('click', handleClickOnTwo);
-  productInsert3.addEventListener('click', handleClickOnThree);
 
 //check for button after 15 clicks
+var showResultButton = document.getElementById('showResultButton');
   function checkForButton() {
-    if(totalClicks < nameArray.length) {
+    if(totalClicks < productArray.length) {
+    button.style.visibility = 'hidden';
+  }
+  else {
+    button.style.visibility = 'visible';
+  }
+}
+
+// BUTTON FUNCTION
+function checkForButton() {
+  if(totalClicks < productArray.length) {
     document.getElementById('button').style.visibility = 'hidden';
   }
   else {
     document.getElementById('button').style.visibility = 'visible';
   }
-}
-//add event listener for button
-  button.addEventListener('click', handleButtonClick);
-//handler for button and results
-function handleButtonClick(event) {
 
+}
+
+button.addEventListener('click', handleButtonClick);
+function handleButtonClick() {
   button.textContent = 'Here are the results';
-  var showResult = document.getElementById('showResult');
-  showResult.textContent = productResults;
-  //create elements for results to show
-  var displayList = document.createElement('ul');
-    for (var i = 0; i < nameArray.length; i++) {
-      nameArray[i].percentClicked();
-      var results = document.createElement('li');
-      productResults.textContent = nameArray[i].name + ' has receieved ' + nameArray[i].timesClicked + ' clicks after being displayed ' + nameArray[i].timesDisplayed + ' times, for a ' + nameArray[i].percentClicked + '% selection rate';
-      displayList.appendChild(results);
-    }
-    resultsDisplay.appendChild(displayList);
-  }
+  dataset1();
+  chart1();
+}
 
 checkForButton();
-handleButtonClick();
+
+
+
 
 //graph
-function renderDataTable() {
-  var getTableEl = document.getElementById('userDataTable');
-  getTableEl.innerHTML = '<h2>Here is the data</h2>';
-}
-var clicksArrayForChart = [];
-
-function makeClickArray() {
-  clicksArrayForChart = [];
-  for(var i = 0; i < nameArray.length; i++) {
-    clicksArrayForChart.push(nameArray[i].totalClicks);
+var label = ['shark', 'chair', 'pen', 'banana', 'bag', 'boots', 'cthulhu', 'dragon', 'sweep', 'scissors', 'unicorn', 'usb', 'water', 'wine'];
+var percentageChart = [];
+function dataset1() {
+  for(var i = 0; i <productArray.length; i++) {
+    percentageChart[i] = productArray[i].percentClicked();
   }
-
 }
 
-// alert('Hello, Please follow the instructions carefully before starting the survey');
+
+
+function chart1(){
+  var data = {
+    labels: label,
+    datasets: [
+      {
+       label: "My First dataset",
+       fillColor: "rgba(220,220,220,0.5)",
+       strokeColor: "rgba(220,220,220,0.8)",
+       highlightFill: "rgba(220,220,220,0.75)",
+       highlightStroke: "rgba(220,220,220,1)",
+       data: percentageChart
+     }
+   ]
+ }
+ var ctx = document.getElementById('userDataTable').getContext('2d');
+ var myBarChart = new Chart(ctx).Bar(data);
+}
